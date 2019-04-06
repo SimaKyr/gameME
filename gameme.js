cc26246fab8cbf909c4f5eb895ec9e1d713303eb
SimaKyr <simaduck@gmail.com>
2019-04-06 15:27:12 +0300

    Upgrade stability

--- gameme.js
+++ gameme.js
@@ -1,12 +1,33 @@
+	  var config = {
+		apiKey: "AIzaSyDnS0sAQ23PL2_Slys6mgRlzJHUGb-7WTU",
+		authDomain: "drawme-9.firebaseapp.com",
+		databaseURL: "https://drawme-9.firebaseio.com",
+		projectId: "drawme-9",
+		storageBucket: "drawme-9.appspot.com",
+		messagingSenderId: "944726631004"
+	  };
+	  firebase.initializeApp(config);
+	  function set(key,value){firebase.database().ref().child(key).set(value);}
+	  get = {};
+	  firebase.database().ref().on('value', snap => get = snap.val());
+      ttt = get.users;
 var register = document.getElementById('register');
 var login = document.getElementById('login');
+var forgotPs = document.getElementById('forgotPs');
+var settings = document.getElementById('settings');
 
 var rb = document.getElementById('rb');
 var lb = document.getElementById('lb');
 var ob = document.getElementById('ob');
+var sb = document.getElementById('sb');
+
+var saveSettings = document.getElementById('saveSettings');
+var fogBtn = document.getElementById('fogBtn');
 
 var logBtn = document.getElementById('logBtn');
 var regBtn = document.getElementById('regBtn');
+
+var fogBtn = document.getElementById('fogBtn');
 
 var nI = document.getElementsByClassName('nI');
 var passI = document.getElementsByClassName('passI');
@@ -14,7 +35,11 @@ var emailI = document.getElementsByClassName('emailI');
 
 var nameD = document.getElementById('name');
 
+var closeSettings = document.getElementById('closeSettings');
+
 var game = document.getElementsByClassName('game');
+
+var btnForgotPassword = document.getElementById('btnForgotPassword');
 
 function reqLogin(){
 login.className = 'form';
@@ -22,19 +47,33 @@ login.className = 'form';
 function reqReg(){
 register.className = 'form';
 }
+function reqFog(){
+forgotPs.className = 'form';
+}
+function reqSet(){
+settings.className = 'form';
+}
 function closeReq(){
 login.className = 'form hide';
 register.className = 'form hide';
+forgotPs.className = 'form hide';
+settings.className = 'form hide';
 }
+closeReq();
 
 lb.onclick = function(){
+closeReq();
 reqLogin();
 }
 
 rb.onclick = function(){
+closeReq();
 reqReg();
 }
-
+sb.onclick = function(){
+closeReq();
+reqSet();
+}
 
 logBtn.onclick = function(){
     if( !emailI[0].validity.valid ){
@@ -43,7 +82,9 @@ logBtn.onclick = function(){
         if(passI[0].value.length < 8){
         alert('Short password! Min length 8');
         }else{
-            firebase.auth().signInWithEmailAndPassword(emailI[0].value, passI[0].value).catch(function(error) {
+            firebase.auth().signInWithEmailAndPassword(emailI[0].value, passI[0].value).then(
+                function(){closeReq();alert('You are login now.');}
+            ).catch(function(error) {
             errorCode = error.code;
             if(errorCode == "auth/wrong-password"){
                 alert('Wrong password!')
@@ -51,7 +92,6 @@ logBtn.onclick = function(){
                 if(errorCode == "auth/invalid-email"){
                     alert('Wrong email!')
                 }else{
-                 window.location.reload();   
                 }
             }
         });
@@ -68,6 +108,7 @@ function httpGet(theUrl)
 }
 
 regBtn.onclick = function(){
+    if(nI[0].value.length<64){
     if(findNickname(nI[0].value)){
         if(Boolean(JSON.parse(httpGet('https://app.verify-email.org/api/v1/otnge6so7NnClqfHmIVPr5wmZ4DxJTIdKHyGfbCkZXTiAEaveL/credits')).credits==0) != Boolean(!JSON.parse(httpGet('https://app.verify-email.org/api/v1/otnge6so7NnClqfHmIVPr5wmZ4DxJTIdKHyGfbCkZXTiAEaveL/verify/' + emailI[1].value)))){
     if( !emailI[1].validity.valid ){
@@ -83,6 +124,8 @@ regBtn.onclick = function(){
     }
 }else{
     alert('This nickname already contains in system!');
+}}else{
+    alert('Don\'t use long nickname!')
 }}
 
 function findNickname(niks){
@@ -101,7 +144,6 @@ function findNickname(niks){
 
 ob.onclick = function(){
 firebase.auth().signOut().then(function() {
-  window.location.reload();
 }).catch(function(error) {
   console.error('Something weird when you trying log out.');
   console.error(String(error));
@@ -112,27 +154,79 @@ closeReq();
 firebase.auth().onAuthStateChanged(function(user) {
 if (firebase.auth().currentUser !== null){
     
-    setTimeout(function(){if(nI[0].value != ""){if(findNickname(nI[0].value)){
+    if(nI[0].value != ""){if(findNickname(nI[0].value)){
         set('users/' + firebase.auth().currentUser.uid + '/nickname',nI[0].value);
 
         set('users/' +'guid' + '/length', (Number(get['users']['guid'].length) + 1).toString());
         set('users/guid/' + get['users']['guid'].length,firebase.auth().currentUser.uid);
-        window.location.reload();
-    }}},3000);
+        closeReq();
+    }}
                 
     ob.className = '';
+    sb.className = '';
     lb.className = 'hide';
     rb.className = 'hide';
     nameD.innerText = 'Logged in ' + firebase.auth().currentUser.email;
     nameD.className = '';
+    setTimeout(function(){while(typeof get.users != 'object'){
+    }
+    while(typeof get.users[firebase.auth().currentUser.uid] != 'object'){
+    }
+    nI[1].value = get.users[firebase.auth().currentUser.uid].nickname;
+    },2000);
     
     game[0].onclick = function(){
         window.open("https://simakyr.github.io/drawMe/","_self");
     }
 }else{
+                    
+    ob.className = 'hide';
+    lb.className = '';
+    rb.className = '';
+    nameD.className = 'hide';
+    sb.className = 'hide';
     game[0].onclick = function(){
         alert('To use games, please login!');
     }
 }
     
 });
+btnForgotPassword.onclick = function(){
+closeReq();
+reqFog();
+}
+fogBtn.onclick = function(){
+        if( !emailI[2].validity.valid ){
+        alert('Valid email!');
+            
+}else{
+        
+firebase.auth().sendPasswordResetEmail(
+    emailI[2].value)
+    .then(function() {
+      closeReq();
+      alert('We send message to you email.');
+    })
+    .catch(function(error) {
+      alert('You put wrong email!');
+    });
+    
+}}
+
+closeSettings.onclick = function(){
+closeReq();
+}
+
+saveSettings.onclick = function(){
+if(nI[1].value.length<64){
+    if(findNickname(nI[1].value)){
+  set('users/' + firebase.auth().currentUser.uid + '/nickname',nI[1].value);
+  closeReq();
+    }
+    else{
+    alert('Nickname using other people!');
+    }
+}else{
+    alert('Very long nickname!')
+}
+}
